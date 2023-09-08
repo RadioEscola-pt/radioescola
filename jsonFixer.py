c0edc8bf340f6db6582b03d990aa7de6ac5413fd
dirtybug <dirtybug@gmail.com>
2023-09-08 23:56:26 +0200

    arranjo questoes 3

--- jsonFixer.py
+++ jsonFixer.py
@@ -7,10 +7,10 @@ def remove_img_tags_and_extract_src(html_content):
     cleanr = re.compile('<img [^>]*src=[\'"]?([^\'" >]+)[\'"]?[^>]*>')
     img_src = re.findall(cleanr, html_content)
     cleaned_html = re.sub(cleanr, '', html_content)
-    return cleaned_html, img_src[0] if img_src else None  # Use the first URL if found
+    return cleaned_html, img_src[0] if img_src else None  # Use the first URL if found, or None if no img tag
 
 # Load the JSON data from a file
-json_filename = 'question3.json'
+json_filename = 'question1.json'
 
 if os.path.exists(json_filename):
     with open(json_filename, 'r', encoding='utf-8') as json_file:
@@ -22,18 +22,26 @@ if os.path.exists(json_filename):
     # Initialize a unique ID counter
     unique_id_counter = 1
 
+    # Initialize a variable to store the highest unique ID
+    highest_unique_id = 0  # Initialize to 0
+
     # Iterate through the questions
     for question in data['questions']:
         question_text = question.get('question', '')
-
         # Add an empty "img" field to questions that don't have it
         if 'img' not in question:
-            question['img'] = None
+            question['img'] = None  # Set "img" to None if missing
+            changes_made = True  # Set changes_made to True
+
 
         # Add a "uniqueID" field with a number sequence to questions that don't have it
         if 'uniqueID' not in question:
             question['uniqueID'] = unique_id_counter
             unique_id_counter += 1
+            changes_made = True  # Set changes_made to True
+
+        # Update the highest_unique_id if a higher unique ID is found
+        highest_unique_id = max(highest_unique_id, question['uniqueID'])
 
         # Remove <img> tags from the question and extract the src attribute
         cleaned_question_text, img_src = remove_img_tags_and_extract_src(question_text)
@@ -44,10 +52,8 @@ if os.path.exists(json_filename):
             question['question'] = cleaned_question_text
 
             # Add a new 'img' field with the extracted image source
-            question['img'] = img_src
-
-            # Set the flag to indicate changes have been made
-            changes_made = True
+            question['img'] = img_src if img_src else None  # Set "img" to None if no img source
+            changes_made = True  # Set changes_made to True
 
             # Display a message indicating the change and print the image
             print(f"Question {question['numb']} has been modified.")
@@ -55,6 +61,9 @@ if os.path.exists(json_filename):
                 print("Image Source(s):")
                 for src in img_src:
                     print(src)
+            else:
+                print("No image source found.")  # Add this message
+
             print(f"UniqueID: {question['uniqueID']}\n")
 
     if changes_made:
@@ -65,6 +74,9 @@ if os.path.exists(json_filename):
         print(f"Saved changes to '{json_filename}'.")
     else:
         print("No changes were made.")
+
+    # Print the highest unique ID
+    print(f"Highest Unique ID: {highest_unique_id}")
 
 else:
     print(f"The file '{json_filename}' does not exist.")
