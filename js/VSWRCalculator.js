6ea00a9605fbed2c9d938c2b554c7fb70dd883a7
Joel Calado <joelcalado@gmail.com>
2023-11-10 08:04:33 +0000

    improve VSWR calculator

--- js/VSWRCalculator.js
+++ js/VSWRCalculator.js
@@ -10,50 +10,56 @@ class VSWRCalculator extends ElectricalUnits {
         this.zlInput = document.getElementById("zl");
         this.zoInput = document.getElementById("zo");
         this.vswrResult = document.getElementById("vswrResult");
-        this.calculateButton = document.getElementById("calculateButton");
+        this.calculationType = document.getElementById("calculationType");
+        this.calculateButton = document.getElementById("calculateBtn");
 
-        this.voltagesRadio = document.getElementById("voltagesRadio");
-        this.impedancesRadio = document.getElementById("impedancesRadio");
-        this.voltagesFields = document.querySelector(".voltages-fields");
-        this.impedancesFields = document.querySelector(".impedances-fields");
+        this.voltagesFields = document.querySelector("#tensions");
+        this.impedancesFields = document.querySelector("#impedances");
 
-        this.voltagesRadio.addEventListener("click", () => this.showVoltagesFields());
-        this.impedancesRadio.addEventListener("click", () => this.showImpedancesFields());
+        this.calculationType.addEventListener("change", () => this.toggleFields());
 
         this.calculateButton.addEventListener("click", () => this.calculateVSWR());
     }
 
-    showVoltagesFields() {
+    toggleFields() {
+        if (this.calculationType.value === "tension") {
+            this.showTensionFields();
+        } else {
+            this.showImpedanceFields();
+        }
+    }
+
+    showTensionFields() {
         this.voltagesFields.classList.remove("hidden");
         this.impedancesFields.classList.add("hidden");
     }
 
-    showImpedancesFields() {
+    showImpedanceFields() {
         this.impedancesFields.classList.remove("hidden");
         this.voltagesFields.classList.add("hidden");
     }
 
     calculateVSWR() {
-        var calcMethod = document.querySelector('input[name="calcMethod"]:checked').value;
+        var calcMethod = this.calculationType.value;
 
-        if (calcMethod === "voltages") {
+        if (calcMethod === "tension") {
             var vmax = parseFloat(this.vmaxInput.value);
             var vmin = parseFloat(this.vminInput.value);
 
             if (!isNaN(vmax) && !isNaN(vmin)) {
                 var vswr = vmax / vmin;
-                this.vswrResult.innerHTML = vswr;
+                this.vswrResult.innerText = "VSWR = " + vswr;
             } else {
                 alert("Por favor, insira valores numéricos válidos para Vmax e Vmin.");
             }
-        } else if (calcMethod === "impedances") {
+        } else if (calcMethod === "impedance") {
             var zl = parseFloat(this.zlInput.value);
             var zo = parseFloat(this.zoInput.value);
 
             if (!isNaN(zl) && !isNaN(zo)) {
                 var reflectionCoefficient = (zl - zo) / (zl + zo);
                 var vswr = (1 + Math.abs(reflectionCoefficient)) / (1 - Math.abs(reflectionCoefficient));
-                this.vswrResult.innerHTML = vswr;
+                this.vswrResult.innerText = "VSWR = " + vswr;
             } else {
                 alert("Por favor, insira valores numéricos válidos para ZL e Zo.");
             }
