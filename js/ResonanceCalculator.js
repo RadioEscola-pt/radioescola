1f62619cb84f27c03518cb7a45de7aebce3b8965
Joel Calado <joelcalado@gmail.com>
2023-11-10 16:12:11 +0000

    improve resonance calculator

--- js/ResonanceCalculator.js
+++ js/ResonanceCalculator.js
@@ -1,41 +1,45 @@
-class ResonanceCalculator extends ElectricalUnits{
-    constructor() {
-        super();
-        new Popup("ResonanceCalculator", this);
+class ResonanceCalculator extends ElectricalUnits {
+  constructor() {
+    super();
+    new Popup("ResonanceCalculator", this);
+  }
+  endRequest() {
+    const calculateButton = document.getElementById("calculate");
+    calculateButton.addEventListener(
+      "click",
+      this.calculateResonance.bind(this)
+    );
+    this.inductanceUnitInput = document.getElementById("inductanceUnit");
+    this.capacitanceUnitInput = document.getElementById("capacitanceUnit");
+    this.hertzUnitInput = document.getElementById("hertzUnit");
+    this.loadOptions(this.inductanceUnitInput, "F");
+    this.loadOptions(this.capacitanceUnitInput, "H");
+  }
 
+  calculateResonance() {
+    const inductance = parseFloat(document.getElementById("inductance").value);
+    const capacitance = parseFloat(
+      document.getElementById("capacitance").value
+    );
 
+    const unitInduValue = this.unitMultipliers[this.inductanceUnitInput.value];
+    const unitCapValue = this.unitMultipliers[this.capacitanceUnitInput.value];
 
+    if (!isNaN(inductance) && !isNaN(capacitance)) {
+      let resonanceFrequency =
+        1 /
+        (2 *
+          Math.PI *
+          Math.sqrt(inductance * unitInduValue * capacitance * unitCapValue));
 
+      document.getElementById(
+        "result"
+      ).textContent = `Frequência de ressonância: ${this.formatNumberWithExponent(
+        resonanceFrequency
+      )} Hz.`;
+    } else {
+      document.getElementById("result").textContent =
+        "Por favor, insira valores válidos para L e C.";
     }
-    endRequest() {
-        const calculateButton = document.getElementById("ReesunaceCalculatorCalculateButton");
-        calculateButton.addEventListener("click", this.calculateResonance.bind(this));
-        this.inductanceUnitInput = document.getElementById("inductanceUnit");
-        this.capacitanceUnitInput = document.getElementById("capacitanceUnit");
-        this.hertzUnitInput = document.getElementById("hertzUnit");
-        this.loadOptions(this.inductanceUnitInput, "F");
-        this.loadOptions(this.capacitanceUnitInput, "H");
-        
-    }
-
-    calculateResonance() {
-        const inductance = parseFloat(document.getElementById("inductance").value);
-        const capacitance = parseFloat(document.getElementById("capacitance").value);
-        
-        
-
-        
-        const unitInduValue = this.unitMultipliers[this.inductanceUnitInput.value];
-        const unitCapValue = this.unitMultipliers[this.capacitanceUnitInput.value];
-        
-
-
-        if (!isNaN(inductance) && !isNaN(capacitance)) {
-            let resonanceFrequency = 1 / (2 * Math.PI * Math.sqrt(inductance*unitInduValue * capacitance*unitCapValue));
-           
-            document.getElementById("result").textContent = `A frequência de ressonância é ${this.formatNumberWithExponent(resonanceFrequency)} Hz.`;
-        } else {
-            document.getElementById("result").textContent = "Por favor, insira valores válidos para L e C.";
-        }
-    }
+  }
 }
