799f6a048246b173833be4ae364a83a9fe514b87
julio <dirtybug@gmail.com>
2024-03-15 13:24:28 +0000

    try to add selay

--- src/cypress/e2e/navigation.fe.cy.js
+++ src/cypress/e2e/navigation.fe.cy.js
@@ -16,65 +16,50 @@ describe("Check navigation", () => {
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("Ganho (dB)").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'O decibel, cujo símbolo é dB,');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-        
         cy.get("#categoria1Dropdown ul li a").contains("Tranformadores").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Um transformador é um dispositivo elétrico');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-  
         cy.get("#categoria1Dropdown ul li a").contains("Amplificadores operacionais").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Os amplificadores operacionais são dispositivos eletrónicos');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("Reactância Indutiva").click();
         cy.wait(2000);
         cy.get("#welcome").should('contain.text', 'Indutores e Reatância Indutiva');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("Reactância Capacitiva").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Condensadores e Reatância Capacitiva');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("Circuitos RL").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Antes de mergulharmos no tópico de adicionar impedância reativa');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("RLC").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Circuito RLC e Impedância');
 
         cy.get("#studyLink").click();
         cy.get("#categoria1Button").click();
-
         cy.get("#categoria1Dropdown ul li a").contains("Receptores").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Recetor Super-regenerativo');
     });
 
@@ -84,28 +69,21 @@ describe("Check navigation", () => {
 
         cy.get("#studyLink").click();
         cy.get("#categoria2Button").click();
-
         cy.get("#categoria2Dropdown ul li a").contains("Bobines").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'O que é uma Bobina?');
 
 
         cy.get("#studyLink").click();
         cy.get("#categoria2Button").click();
-        
         cy.get("#categoria2Dropdown ul li a").contains("Capacidade de baterias").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Capacidade de uma bateria');
 
         cy.get("#studyLink").click();
         cy.get("#categoria2Button").click();
-
-  
         cy.get("#categoria2Dropdown ul li a").contains("Onda estacionária (SWR)").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'VSWR é definido como a razão entre as ondas estacionárias ');
     });
 
@@ -115,82 +93,62 @@ describe("Check navigation", () => {
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-        
         cy.get("#categoria3Dropdown ul li a").contains("Alfabeto Fonético").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Alfabeto Fonético');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-        
         cy.get("#categoria3Dropdown ul li a").contains("Abreviaturas de Operação").click();
         cy.wait(2000);
-
-        cy.get("#welcome").should('contain.text', 'Abreviaturas de Operação');
+        cy.get("#welcome").should('contain.text', 'Abreviaturas de Operação');~
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Definições Genéricas").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Definições genéricas');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Entidades e Competências").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Entidades e suas competências');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Código Q").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Código Q');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Prefixos de indicativos").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Lista dos prefixos de indicativos de chamada');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Lei de Ohm").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Lei de Ohm');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Corrente Alternada / AC").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Corrente Alternada / AC');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Código de cores").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Código de cores para resistências');
 
         cy.get("#studyLink").click();
         cy.get("#categoria3Button").click();
-  
         cy.get("#categoria3Dropdown ul li a").contains("Mult e SubMult. de Unidades").click();
         cy.wait(2000);
-
         cy.get("#welcome").should('contain.text', 'Múltiplos e submúltiplos de unidades');
       });
 
